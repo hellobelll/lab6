2c3e2cbaed6bd7df340b252c78e8483e4a939b9a
belll_ <ess.fuji5566@gmail.com>
2025-12-09 16:49:41 +0700

    "lab6_1.cpp completed"

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,17 @@
 #include<iostream>
 using namespace std;
-
-int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
-    return 0;
-}
+int main() {
+    int x=1,even=0,odd=0;
+    while (x!=0){
+        cout << "Enter an integer: ";
+        cin >> x;
+        if (x%2==0&&x!=0)
+        {
+        even++;
+        }else if(x%2!=0&&x!=0){
+        odd++;
+    }
+    }
+    cout << "#Even numbers = "<< even << endl;
+    cout << "#Odd numbers = "<< odd;    
+}   
